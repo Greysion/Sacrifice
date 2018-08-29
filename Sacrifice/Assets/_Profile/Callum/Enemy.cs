b679a8c7f9ab7d404385141145412fad79d9e773
SAE\cbrown33 <1009488@student.sae.edu.au>
2018-08-29 14:56:04 +0800

    Added EnemyPrefab/scripts

---
+++ Sacrifice/Assets/_Profile/Callum/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float health;
+    public float speed;
+    public float damage;
+    public Transform player;
+
+}
---
+++ Sacrifice/Assets/_Profile/Callum/Enemy_Runner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Runner : Enemies {
+
+    public Transform playerPos;
+
+	void Update () {
+        transform.LookAt(playerPos);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+	}
+}
