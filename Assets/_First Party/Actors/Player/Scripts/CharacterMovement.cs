4851a419cd8aff8ea0b99f8ef548c4261231ffd6
Hayden Reeve <haydenreeve@hotmail.com>
2018-09-03 01:59:17 +0800

    Air Movement Enabled
    • The player can now change velocity mid-air.

--- Assets/_First Party/Actors/Player/Scripts/CharacterMovement.cs
+++ Assets/_First Party/Actors/Player/Scripts/CharacterMovement.cs
@@ -74,11 +74,12 @@ public class CharacterMovement : MonoBehaviour {
 	/// <param name="acceleration">The speed at which the player is going to move.</param>
 	public void Direction(float direction = 0f, float acceleration = 1f) {
 
+		moveDirection = new Vector3(direction * acceleration, 0f, moveDirection.z);
+
 		if (!Grounded())
 			return;
 
-		visuals.Jumping(false);
-		moveDirection = new Vector3(direction * acceleration, 0f, moveDirection.z);		
+		visuals.Jumping(false);		
 
 	}
 
